be3548dd828e9d07676a1385c41f92a3b76dc3c5
Samir Saeedi <thesdev@outlook.com>
2020-12-18 13:20:17 +0330

    Use custom date picker and switch to 'controlled components'

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -5,10 +5,13 @@ import { useState } from "react";
 function DatePicker({
     value,
     handleChange,
+    className,
 }: {
     value: Date;
     handleChange: Function;
+    className: string;
 }) {
+    const [pickerShown, setPickerShown] = useState(false);
     const [start, setStart] = useState(startOfMonth(value));
     const end = endOfMonth(start);
 
@@ -33,97 +36,104 @@ function DatePicker({
         setStart(addMonths(start, change));
     }
 
-    return (
-        <div>
-            <span className="text-gray-500">Date Range Picker</span>
-            <div className="flex justify-between mt-8">
-                <button
-                    className="text-gray-500"
-                    type="button"
-                    onClick={() => handleMonthChange(-1)}
-                >
-                    <svg
-                        className="w-6 h-6"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M15 19l-7-7 7-7"
-                        />
-                    </svg>
-                </button>
-                <span className="font-bold">{`${
-                    months[start.getMonth()]
-                } ${start.getFullYear()}`}</span>
-                <button
-                    className="text-gray-500"
-                    type="button"
-                    onClick={() => handleMonthChange(1)}
-                >
-                    <svg
-                        className="w-6 h-6"
-                        fill="none"
-                        stroke="currentColor"
-                        viewBox="0 0 24 24"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M9 5l7 7-7 7"
-                        />
-                    </svg>
-                </button>
-            </div>
+    function handleInputClick() {
+        setPickerShown(true);
+    }
 
-            <table className="w-full mt-6 text-center table-fixed">
-                <thead>
-                    <tr className="font-bold text-gray-500 border-b">
-                        {weekDaysInitials.map((o, i) => (
-                            <th className="w-12 h-10" key={i}>
-                                {o}
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody>
-                    {dates.map((o, i) => (
-                        <tr key={"row-" + i}>
-                            {o.map((p, j) => (
-                                <td className="w-12 h-12" key={"cell-" + j}>
-                                    {p ? (
-                                        <button
-                                            className={`w-full h-full hover:bg-blue-100 text-gray-700 rounded-full ${
-                                                p.getDate() ===
-                                                    value.getDate() &&
-                                                p.getMonth() ===
-                                                    value.getMonth() &&
-                                                p.getFullYear() ===
-                                                    value.getFullYear()
-                                                    ? "font-bold bg-yellow-100 text-yellow-500"
-                                                    : ""
-                                            }`}
-                                            type="button"
-                                            onClick={() => handleChange(p)}
-                                        >
-                                            {p.getDate()}
-                                        </button>
-                                    ) : (
-                                        p
-                                    )}
-                                </td>
+    return (
+        <>
+            <span onClick={handleInputClick} className={className}>{value.toLocaleDateString()}</span>
+            <div className="p-6 border">
+                <span className="text-gray-500">Date Range Picker</span>
+                <div className="flex justify-between mt-8">
+                    <button
+                        className="text-gray-500"
+                        type="button"
+                        onClick={() => handleMonthChange(-1)}
+                    >
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M15 19l-7-7 7-7"
+                            />
+                        </svg>
+                    </button>
+                    <span className="font-bold">{`${
+                        months[start.getMonth()]
+                    } ${start.getFullYear()}`}</span>
+                    <button
+                        className="text-gray-500"
+                        type="button"
+                        onClick={() => handleMonthChange(1)}
+                    >
+                        <svg
+                            className="w-6 h-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M9 5l7 7-7 7"
+                            />
+                        </svg>
+                    </button>
+                </div>
+
+                <table className="w-full mt-6 text-center table-fixed">
+                    <thead>
+                        <tr className="font-bold text-gray-500 border-b">
+                            {weekDaysInitials.map((o, i) => (
+                                <th className="w-12 h-10" key={i}>
+                                    {o}
+                                </th>
                             ))}
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody>
+                        {dates.map((o, i) => (
+                            <tr key={"row-" + i}>
+                                {o.map((p, j) => (
+                                    <td className="w-12 h-12" key={"cell-" + j}>
+                                        {p ? (
+                                            <button
+                                                className={`w-full h-full hover:bg-blue-100 text-gray-700 rounded-full ${
+                                                    p.getDate() ===
+                                                        value.getDate() &&
+                                                    p.getMonth() ===
+                                                        value.getMonth() &&
+                                                    p.getFullYear() ===
+                                                        value.getFullYear()
+                                                        ? "font-bold bg-yellow-100 text-yellow-500"
+                                                        : ""
+                                                }`}
+                                                type="button"
+                                                onClick={() => handleChange(p)}
+                                            >
+                                                {p.getDate()}
+                                            </button>
+                                        ) : (
+                                            p
+                                        )}
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 }
 
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,4 +1,5 @@
-import { FormEvent } from "react";
+import {setDate, setHours, setMinutes, setMonth, setYear} from "date-fns";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { Action } from "../redux";
@@ -8,17 +9,35 @@ import DatePicker from "./DatePicker";
 function Todo() {
     const dispatch = useDispatch();
     const history = useHistory();
+    const todo = new TodoEntity("Todo", new Date(), TodoStatus.InProgress);
+    const [model, setModel] = useState(todo);
+
+    function handleTitleChange(e: ChangeEvent<HTMLInputElement>) {
+        setModel({ ...model, title: e.target.value });
+    }
+
+    function handleStatusChange(e: ChangeEvent<HTMLSelectElement>) {
+        setModel({
+            ...model,
+            status: TodoStatus[e.target.value as keyof typeof TodoStatus],
+        });
+    }
+
+    function handleDateChange(e: Date) {
+        const date = setYear(setMonth(setDate(model.date, e.getDate()), e.getMonth()), e.getFullYear());
+        setModel({ ...model, date });
+    }
+
+    function handleTimeChange(e: ChangeEvent<HTMLInputElement>) {
+        const timeParts = e.target.value.split(":");
+        const date = setHours(setMinutes(model.date, Number(timeParts[1] || "")), Number(timeParts[0]));
+        setModel({ ...model, date});
+    }
+
 
     function handleSubmit(e: FormEvent) {
         e.preventDefault();
-        const { title, status, date, time } = (e.target as HTMLFormElement)
-            .elements as any;
-        const todo = new TodoEntity(
-            title.value,
-            new Date(`${date.value} ${time.value}`),
-            status.value
-        );
-        const action = { ...new Action("TodoAdd", todo) };
+        const action = { ...new Action("TodoAdd", model) };
         dispatch(action);
         history.push("/todos/to-do/month");
     }
@@ -35,10 +54,17 @@ function Todo() {
                     type="text"
                     id="title"
                     className="p-1 mt-2 mb-6 border rounded"
+                    value={model.title}
+                    onChange={handleTitleChange}
                 />
 
                 <label htmlFor="status">Status</label>
-                <select id="status" className="p-1 mt-2 mb-6 border rounded">
+                <select
+                    value={model.status}
+                    onChange={handleStatusChange}
+                    id="status"
+                    className="p-1 mt-2 mb-6 border rounded"
+                >
                     {Object.values(TodoStatus).map((o) => (
                         <option key={o} value={o}>
                             {o}
@@ -47,23 +73,21 @@ function Todo() {
                 </select>
 
                 <label htmlFor="date">Date</label>
-                <input
-                    type="date"
-                    id="date"
+                <DatePicker
                     className="p-1 mt-2 mb-6 border rounded"
-                />
+                    value={model.date}
+                    handleChange={handleDateChange}
+                ></DatePicker>
 
                 <label htmlFor="time">Time</label>
                 <input
                     type="time"
                     id="time"
+                    value={`${model.date.getHours().toString().padStart(2, "0")}:${model.date.getMinutes().toString().padStart(2, "0")}`}
+                    onChange={handleTimeChange}
+                    required
                     className="p-1 mt-2 mb-6 border rounded"
                 />
-
-                <DatePicker
-                    value={new Date()}
-                    handleChange={() => {}}
-                ></DatePicker>
 
                 <button
                     className="p-1 text-white bg-green-600 border rounded"
